2bc5aa5a0843fe80b9f0028808519ac55f6ecf43
Pedro Batista da Silva <pbs.multimedia@gmail.com>
2021-08-01 02:04:47 +0100

    - clean up

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
     <h1 class="location-title">Uyo, Nigeria - 12 hours weather forecast</h1>
-    <GreyLayer />
     <Sky />
     <Soil />
     <Rain v-if="weather.rainProbability > 33" />
@@ -13,7 +12,6 @@
 import { mapState } from "vuex";
 import Sky from "./components/Sky";
 import Soil from "./components/Soil";
-import GreyLayer from "./components/GreyLayer";
 import Rain from "./components/Rain";
 import Data from "./components/Data";
 
@@ -22,7 +20,6 @@ export default {
   components: {
     Sky,
     Soil,
-    GreyLayer,
     Rain,
     Data,
   },
@@ -43,23 +40,5 @@ body {
   color: #fff;
   text-align: center;
   opacity: 0.5;
-}
-
-#app {
-}
-
-.test {
-  position: absolute;
-  top: 200px;
-  margin: 0 auto;
-  left: 0;
-  right: 0;
-  width: 200px;
-  height: 200px;
-  border: 1px solid #ccc;
-  padding: 15px;
-  background: rgba(255 255 255 / 50%);
-  backdrop-filter: blur(10px);
-  border-radius: 5px;
 }
 </style>
